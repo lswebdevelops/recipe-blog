dd7fb76327a543920e99f12af395e61bc8edaa82
lswebdevelops <sardanhaluciano9690@gmail.com>
2024-01-11 22:09:02 -0300

    adding recipes to db, adding thai recipes do homepage

---
+++ server/models/database.js
@@ -0,0 +1,14 @@
+const mongoose = require("mongoose");
+mongoose.connect(process.env.MONGODB_URI, {
+});
+
+const db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error'));
+db.once('open', ()=> {
+    console.log('Connected');
+})
+
+// models
+
+require('./Category')
+require('./Recipe')
--- server/routes/recipeRoutes.js
+++ server/routes/recipeRoutes.js
@@ -9,6 +9,7 @@ const recipeController = require('../controllers/recipeController')
  */
 
 router.get('/', recipeController.homepage);
+router.get('/categories', recipeController.exploreCategories);
 
 module.exports = router;
 
